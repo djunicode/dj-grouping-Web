8d86a2d798f86d3bf5ea005573f6742bdac9f495
Rachana1104 <y.rachana1104@gmail.com>
2022-05-13 00:37:23 +0530

    restored the changes lost in merge conflicts

--- frontend/src/pages/Create_profile/Profile.js
+++ frontend/src/pages/Create_profile/Profile.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Typography, Card, CardContent, Box } from "@mui/material";
+import { Typography, Card, CardContent, Box, Grid, Button } from "@mui/material";
 import "./Profile.scss";
 
 const Profile = () => {
@@ -25,14 +25,39 @@ const Profile = () => {
               <Typography variant="h5" className="profile_subheading">
                 Select your Interests
               </Typography>
-              <div className="profile_interests">Photography</div>
+              <Box sx={{ flexGrow: 1 }}>
+                <Grid
+                  container
+                  spacing={{ xs: 2, md: 5 }}
+                  columns={{ xs: 4, sm: 8, md: 12 }}
+                >
+                  {Array.from(Array(8)).map((_, index) => (
+                    <Grid item xs={2} sm={4} md={3} key={index}>
+                      <div className="profile_interests">Photography</div>
+                    </Grid>
+                  ))}
+                </Grid>
+              </Box>
             </div>
             <div className="profile_otherintersts_div">
               <Typography variant="h5" className="profile_subheading">
                 Other Interests
               </Typography>
-              <div className="profile_interests">Photography</div>
+              <Box sx={{ flexGrow: 1 }}>
+                <Grid
+                  container
+                  spacing={{ xs: 2, md: 5 }}
+                  columns={{ xs: 4, sm: 8, md: 12 }}
+                >
+                  {Array.from(Array(8)).map((_, index) => (
+                    <Grid item xs={2} sm={4} md={3} key={index}>
+                      <div className="profile_interests">Photography</div>
+                    </Grid>
+                  ))}
+                </Grid>
+              </Box>
             </div>
+            <Button className="profile_button">Proceed</Button>
           </CardContent>
         </Card>
       </Box>
