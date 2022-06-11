560885571b6242695ceb4fe8c47c3eafa21e4101
Rachana1104 <y.rachana1104@gmail.com>
2022-06-11 22:52:43 +0530

    navbar added

--- frontend/src/pages/Events/Events.js
+++ frontend/src/pages/Events/Events.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
@@ -24,101 +24,107 @@ const Events = () => {
     dispatch(ViewEvents());
   }, [dispatch]);
 
-  useEffect(()=>{
-    console.log(userEvents)
-    if(userEvents){
-      setData(userEvents)
+  useEffect(() => {
+    console.log(userEvents);
+    if (userEvents) {
+      setData(userEvents);
     }
-  },[userEvents]);
+  }, [userEvents]);
 
-useEffect(()=>{
-  console.log(data.length)
-},[data]);
+  useEffect(() => {
+    console.log(data.length);
+  }, [data]);
 
   return (
-    <div className="events">
-      {loading ? (
-        <h3>Loading...</h3>
-      ) : error ? (
-        <h3>error</h3>
-      ) : (
-        <>
-          <Typography gutterBottom variant="h5" component="div">
-            Upcoming Events
-          </Typography>
-          <center>
-            <Box sx={{ flexGrow: 1 }} className="events_box">
-              <Grid
-                container
-                spacing={{ xs: 2, md: 3 }}
-                columns={{ xs: 4, sm: 8, md: 12 }}
-              >
-                {data.map((_, index) => (
-                  <Grid item xs={6} sm={4} md={3} key={index}>
-                    <Card className="events_card">
-                      <CardActionArea>
-                        <CardMedia
-                          component="img"
-                          height="140"
-                          image="https://images.unsplash.com/photo-1537824598505-99ee03483384?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8c2NlbmVyeXxlbnwwfHwwfHw%3D&w=1000&q=80"
-                          alt=""
-                          className="events_image"
-                        />
-                        <CardContent>
-                          <Divider></Divider>
-                          <Typography
-                            gutterBottom
-                            variant="h6"
-                            component="div"
-                            className="events_name"
-                          >
-                            {_.event_name}                           
-                          </Typography>
-                        </CardContent>
-                      </CardActionArea>
-                    </Card>
-                  </Grid>
-                ))}
-              </Grid>
-            </Box>
-          </center>
-          <Typography gutterBottom variant="h5" component="div">
-            Hackathons
-          </Typography>
-          <center>
-            <Box sx={{ flexGrow: 1 }} className="events_box">
-              <Grid
-                container
-                spacing={{ xs: 2, md: 3 }}
-                columns={{ xs: 4, sm: 8, md: 12 }}
-              >
-                {data.map((_, index) => (
-                  <Grid item xs={6} sm={4} md={3} key={index}>
-                    <Card className="events_card">
-                      <CardActionArea>
-                        <CardMedia
-                          component="img"
-                          height="140"
-                          image="https://images.unsplash.com/photo-1537824598505-99ee03483384?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8c2NlbmVyeXxlbnwwfHwwfHw%3D&w=1000&q=80"
-                          alt=""
-                          className="events_image"
-                        />
-                        <CardContent>
-                          <Divider></Divider>
-                          <Typography gutterBottom variant="h6" component="div">
-                          {_.event_name}
-                          </Typography>
-                        </CardContent>
-                      </CardActionArea>
-                    </Card>
-                  </Grid>
-                ))}
-              </Grid>
-            </Box>
-          </center>
-        </>
-      )}
-    </div>
+    <>
+      <div className="events">
+        {loading ? (
+          <h3>Loading...</h3>
+        ) : error ? (
+          <h3>error</h3>
+        ) : (
+          <>
+            <Typography gutterBottom variant="h5" component="div">
+              Upcoming Events
+            </Typography>
+            <center>
+              <Box sx={{ flexGrow: 1 }} className="events_box">
+                <Grid
+                  container
+                  spacing={{ xs: 2, md: 3 }}
+                  columns={{ xs: 4, sm: 8, md: 12 }}
+                >
+                  {data.map((_, index) => (
+                    <Grid item xs={6} sm={4} md={3} key={index}>
+                      <Card className="events_card">
+                        <CardActionArea>
+                          <CardMedia
+                            component="img"
+                            height="140"
+                            image="https://images.unsplash.com/photo-1537824598505-99ee03483384?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8c2NlbmVyeXxlbnwwfHwwfHw%3D&w=1000&q=80"
+                            alt=""
+                            className="events_image"
+                          />
+                          <CardContent>
+                            <Divider></Divider>
+                            <Typography
+                              gutterBottom
+                              variant="h6"
+                              component="div"
+                              className="events_name"
+                            >
+                              {_.event_name}
+                            </Typography>
+                          </CardContent>
+                        </CardActionArea>
+                      </Card>
+                    </Grid>
+                  ))}
+                </Grid>
+              </Box>
+            </center>
+            <Typography gutterBottom variant="h5" component="div">
+              Hackathons
+            </Typography>
+            <center>
+              <Box sx={{ flexGrow: 1 }} className="events_box">
+                <Grid
+                  container
+                  spacing={{ xs: 2, md: 3 }}
+                  columns={{ xs: 4, sm: 8, md: 12 }}
+                >
+                  {data.map((_, index) => (
+                    <Grid item xs={6} sm={4} md={3} key={index}>
+                      <Card className="events_card">
+                        <CardActionArea>
+                          <CardMedia
+                            component="img"
+                            height="140"
+                            image="https://images.unsplash.com/photo-1537824598505-99ee03483384?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8c2NlbmVyeXxlbnwwfHwwfHw%3D&w=1000&q=80"
+                            alt=""
+                            className="events_image"
+                          />
+                          <CardContent>
+                            <Divider></Divider>
+                            <Typography
+                              gutterBottom
+                              variant="h6"
+                              component="div"
+                            >
+                              {_.event_name}
+                            </Typography>
+                          </CardContent>
+                        </CardActionArea>
+                      </Card>
+                    </Grid>
+                  ))}
+                </Grid>
+              </Box>
+            </center>
+          </>
+        )}
+      </div>
+    </>
   );
 };
 export default Events;
